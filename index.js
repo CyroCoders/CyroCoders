d9367a18272ef46d616968453e1e43fb0178309f
sushant <sushant128815@gmail.com>
2021-06-14 15:10:43 +0530

    Added Join Link And Fixed Click Area

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 window.addEventListener('load', function(){
     document.querySelector(':root').style.setProperty("--server-statistics-box", (200 - 0) * (document.getElementById("server-statistics-box").offsetHeight - 0) / (470 - 0) + 70 + "px");
+    document.querySelector(':root').style.setProperty("--top-div-padding",document.getElementById("server-statistics-box").offsetHeight-((200 - 0) * (document.getElementById("server-statistics-box").offsetHeight - 0) / (470 - 0) + 70) + "px");
     document.querySelector(':root').style.setProperty("--screen-diagonal", Math.sqrt(document.body.offsetHeight*document.body.offsetHeight + document.body.offsetWidth*document.body.offsetWidth) + "px");
     document.querySelector(':root').style.setProperty("--corner-diagonal-angle", Math.asin(document.querySelector(':root').style.getPropertyValue("corner-diagonal")/document.body.offsetHeight));
 });
+var winHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 console.log(window.scrollY)
 
 console.log(Math.sqrt(document.body.offsetHeight*document.body.offsetHeight + document.body.offsetWidth*document.body.offsetWidth)/document.body.offsetHeight+"deg")
@@ -12,8 +14,36 @@ fetch('https://serverstat.sushantshah.repl.co/membercount/842428958397038663').t
 document.addEventListener('scroll', (event) =>{
     if (window.scrollY > 575){
         document.getElementsByClassName("div-fs")[0].style.position = "unset";
+        if(getScrollSpeed() > 50){
+            //document.getElementsByClassName("div-fs")[2].scrollIntoView()
+        }
     } else {
         document.getElementsByClassName("div-fs")[0].style.position = "fixed";
 
     }
 })
+
+var getScrollSpeed = (function(settings){
+    settings = settings || {};
+
+    var lastPos, newPos, timer, delta, 
+        delay = settings.delay || 50; // in "ms" (higher means lower fidelity )
+
+    function clear() {
+      lastPos = null;
+      delta = 0;
+    }
+
+    clear();
+
+    return function(){
+      newPos = window.scrollY;
+      if ( lastPos != null ){ // && newPos < maxScroll 
+        delta = newPos -  lastPos;
+      }
+      lastPos = newPos;
+      clearTimeout(timer);
+      timer = setTimeout(clear, delay);
+      return delta;
+    };
+})();
